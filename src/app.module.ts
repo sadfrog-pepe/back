c4597babed319b9110c988b1947df44c7d7187a3
DevJay1024 <devjay1024@gmail.com>
2022-10-23 17:11:33 +0900

    Feat: DB연결 및 default entity 변경
    - DB 연결
    - default entity 중에서 CreatedUpdatedDateColumnEntity를 CreatedDateColumnEntity로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,44 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import path from "path";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { LoggerMiddleware } from "./middlewares/logger.middleware";
+import { UsersModule } from "./modules/users.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService, ConfigService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		TypeOrmModule.forRootAsync({
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => {
+				return {
+					retryAttempts: configService.get("env") === "prod" ? 10 : 1,
+					type: "mysql",
+					host: configService.get("DB_HOST") || "localhost",
+					port: configService.get("DB_PORT") || 3306,
+					database: configService.get("DB_NAME") || "vintage",
+					username: configService.get("DB_USER") || "root",
+					password: configService.get("DB_PASSWORD") || "",
+					entities: [
+						path.join(__dirname, "entities/**/*.entity.ts"),
+						path.join(__dirname, "entities/**/*.entity.js"),
+					],
+					synchronize: true,
+					logging: false,
+				};
+			},
+		}),
+		UsersModule,
+	],
+	controllers: [AppController],
+	providers: [AppService, ConfigService],
 })
 export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): any {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
-  }
+	configure(consumer: MiddlewareConsumer): any {
+		consumer.apply(LoggerMiddleware).forRoutes("*");
+	}
 }
