0c380a85ad8b225f1eccf64fc8c8398964571aca
DevJay1024 <devjay1024@gmail.com>
2022-10-25 19:36:13 +0900

    Chore: dto 생성 및 이름 변경
    - join.request.dto -> create.user.api.dto
    - loign.user.api.dto 생성

---
+++ src/dto/create.user.api.dto.ts
@@ -0,0 +1,9 @@
+export class CreateUserApiDto {
+	public email: string;
+	public password: string;
+	public gender: string;
+	public height: string;
+	public weight: string;
+	public phone: string;
+	public consentMarketing: string;
+}
--- src/dto/join.request.dto.ts
+++
@@ -1,4 +0,0 @@
-export class JoinRequestDto {
-    public email: string;
-    public password: string;
-}
