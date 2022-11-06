b4dfa265da9afe187b84060692663a0d18e678aa
DevJay1024 <devjay1024@gmail.com>
2022-11-06 18:45:03 +0900

    refactor: 메인페이지 http요청에 반환 문자열을 main-page로 변경 및 api 문서 설명 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from "@nestjs/common";
+import { ApiDefaultResponse, ApiOperation } from "@nestjs/swagger";
 import { AppService } from "./app.service";
 
 @Controller()
@@ -6,7 +7,16 @@ export class AppController {
 	constructor(private readonly appService: AppService) {}
 
 	@Get()
-	getHello(): string {
-		return this.appService.getHello();
+	@ApiOperation({
+		summary: "메인 페이지",
+		description: "메인 페이지, 미구현",
+	})
+	@ApiDefaultResponse({
+		description:
+			"아무 것도 없는 메인 페이지, main-page 문자열을 response한다.",
+		type: String,
+	})
+	getMainPage(): string {
+		return this.appService.getMainPage();
 	}
 }
