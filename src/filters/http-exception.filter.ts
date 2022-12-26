98617856add79fe1b6a358a3b4b7d981afb27ae2
DevJay1024 <devjay1024@gmail.com>
2022-12-27 04:39:11 +0900

    fix: HttpExceptionFilter에 에러 상황 메시지 미구현 오류 수정

--- src/filters/http-exception.filter.ts
+++ src/filters/http-exception.filter.ts
@@ -1,25 +1,26 @@
 import {
-	ArgumentsHost,
-	Catch,
-	ExceptionFilter,
-	HttpException,
+    ArgumentsHost,
+    Catch,
+    ExceptionFilter,
+    HttpException,
 } from "@nestjs/common";
 import { Response } from "express";
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-	catch(exception: HttpException, host: ArgumentsHost) {
-		const ctx = host.switchToHttp();
-		const request = ctx.getRequest<Request>();
-		const response = ctx.getResponse<Response>();
-		const status = exception.getStatus();
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const request = ctx.getRequest<Request>();
+        const response = ctx.getResponse<Response>();
+        const status = exception.getStatus();
 
-		console.log(exception.message);
+        console.log(host + " " + exception.message);
 
-		response.status(status).json({
-			statusCode: status,
-			timestamp: new Date().toISOString(),
-			path: request.url,
-		});
-	}
+        response.status(status).json({
+            statusCode: status,
+            timestamp: new Date().toISOString(),
+            path: request.url,
+            messgae: exception.message,
+        });
+    }
 }
