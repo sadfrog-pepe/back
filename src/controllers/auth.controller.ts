713edbf4509ff0548c8d3a8c432362df6d2fdcc7
DevJay1024 <devjay1024@gmail.com>
2022-12-27 22:43:34 +0900

    feat: 유저 로그인 인증 토큰을 쿠키에 저장하는 기능 추가
    인증 토큰을 response body에 그냥 주는 것은 auths/login이고, 인증 토큰을 response cookie에 주는 것은 auths/login-cookie이다

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -25,7 +25,6 @@ import {
     ApiUnauthorizedResponse,
     PickType,
 } from "@nestjs/swagger";
-import { UserService } from "src/services/user.service";
 import { UserEntity } from "src/entities/user.entity";
 import { User } from "src/decorators/user.decorator";
 
@@ -35,15 +34,16 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @ApiOperation({
-        summary: "로그인(local 인증) api",
+        summary: "로그인(local 인증) api v1",
         description:
-            "유저의 이메일과 비밀번호가 데이터베이스와 동일하면 인증에 성공한다.",
+            "유저의 이메일과 비밀번호가 데이터베이스와 동일하면 인증에 성공하며, token를 response body에 기술한다.",
     })
     @ApiBody({
         type: LoginUserDto,
     })
     @ApiOkResponse({
-        description: "access token을 제공한다",
+        description:
+            "access token을 Response Body에 제공한다. {'accessToken': 'asdfas'}",
         type: String,
     })
     @ApiUnauthorizedResponse({
@@ -55,6 +55,35 @@ export class AuthController {
     async login(@User() user: UserEntity) {
         const accessToken = await this.authService.tokenize(user);
         return { accessToken };
+    }
+
+    @ApiOperation({
+        summary: "로그인(local 인증) api v2",
+        description:
+            "유저의 이메일과 비밀번호가 데이터베이스와 동일하면 인증에 성공하며, token을 쿠키에 저장한다.",
+    })
+    @ApiBody({
+        type: LoginUserDto,
+    })
+    @ApiOkResponse({
+        description:
+            "access token을 Response Cookie에 제공하고, {'message': 'success'}를 body에 반환한다.",
+        type: String,
+    })
+    @ApiUnauthorizedResponse({
+        description:
+            "에러 발생 일시, 에러 메시지, 에러가 발생된 Path, status code를 반환한다.",
+    })
+    @UseGuards(LocalAuthGuard)
+    @Post("login-cookie")
+    async loginNew(@User() user: UserEntity, @Res() res: Response) {
+        const accessToken = await this.authService.tokenize(user);
+        res.cookie("accessToken", accessToken, {
+            maxAge: 600 * 1000,
+            httpOnly: true,
+        }).send({
+            message: "success",
+        });
     }
 
     @ApiOperation({
