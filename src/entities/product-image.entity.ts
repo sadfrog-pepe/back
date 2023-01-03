8fb079e2beea6597252c3d60e27d674e5629af42
DevJay1024 <devjay1024@gmail.com>
2023-01-03 22:38:41 +0900

    feat: 상품에 관련된 api 정의

--- src/entities/product-image.entity.ts
+++ src/entities/product-image.entity.ts
@@ -1,3 +1,5 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsUrl } from "class-validator";
 import { Entity, Column, ManyToOne, JoinColumn } from "typeorm";
 import { CommonEntity } from "./common/common.entity";
 import { ProductEntity } from "./product.entity";
@@ -12,12 +14,26 @@ export type ImageType = keyof typeof ImageType;
 
 @Entity()
 export class ProductImageEntity extends CommonEntity {
-    @Column()
+    @ApiProperty({
+        description: "어떠한 상품에 대한 이미지인지 알려주는 상품의 id값",
+        example: 4,
+    })
+    @Column({ name: "productId" })
     productId: number;
 
+    @ApiProperty({
+        description: "어떠한 상품에 대한 이미지인지 알려주는 상품의 id값",
+        examples: ["Main", "Sub", "Thumbnail"],
+    })
+    @IsNotEmpty()
     @Column()
     imageType: ImageType;
 
+    @ApiProperty({
+        description: "상품이 저장된 AWS S3 url path",
+        example: "https://vinzip.kr/web/product/big//38/A7164.jpg",
+    })
+    @IsUrl()
     @Column()
     url: string;
 
@@ -25,7 +41,7 @@ export class ProductImageEntity extends CommonEntity {
      * below are relations
      */
 
-    @ManyToOne(() => ProductEntity, (p) => p.images)
+    @ManyToOne(() => ProductEntity, (product) => product.images)
     @JoinColumn({ name: "productId", referencedColumnName: "id" })
     product: ProductEntity;
 }
